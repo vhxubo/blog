93e47ad6344c2af58b75df4bb911bcba8a8a53e5
vhxubo <vhxubo@gmail.com>
2022-01-24 22:56:21 +0800

    perf(font): preload web fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
