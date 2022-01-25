fccd3e37ee42b61356c9dc86f299339656b4eb5e
vhxubo <vhxubo@gmail.com>
2022-01-25 16:46:30 +0800

    fix: date timezone

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,21 @@
 const CleanCSS = require("clean-css");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 const timezone = require("dayjs/plugin/timezone");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
-  eleventyConfig.addFilter("datetz", function (date) {
-    dayjs.extend(utc);
-    dayjs.extend(timezone);
-    return dayjs.tz(date, "Asia/Shanghai").format("YYYY-MM-DD HH:mm:ss");
+  eleventyConfig.addFilter("datetzday", function (date) {
+    return dayjs(date).tz("Asia/Shanghai").format("YYYY-MM-DD");
+  });
+  eleventyConfig.addFilter("datetztime", function (date) {
+    return dayjs(date).tz("Asia/Shanghai").format("YYYY-MM-DD HH:mm");
   });
   eleventyConfig.addPlugin(syntaxHighlight);
   return {
